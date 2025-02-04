ca1a23e79f8220d07e8ae30d8b8f54bd2a2d9657
Abdul Rahman <abdulnsamba@gmail.com>
2025-02-04 22:21:14 +0300

    added login

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('create/', views.create, name="create-room"),
     path('update/<int:id>', views.update, name="update-room"),
     path('delete/<int:id>', views.delete, name="delete"),
+    path("login", views.login_page, name="login_page"),
+    path("logout", views.logout_page, name="logout_page"),
+    path("register", views.register, name="register")
 ]
--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render, redirect
 from .models import Room, Topic, User
 from django.db.models import Q
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 # from .form import RoomForm
 
 # Create your views here.
 
+
 def index(request):
-    
+
     topics = Topic.objects.all()
+    room_count = Room.objects.count()
 
     q = request.GET.get('q', '').strip()
 
@@ -18,27 +24,73 @@ def index(request):
             Q(name__icontains=q) |
             Q(description__icontains=q)
         ).order_by("-created")
+        room_count = rooms.count()
     else:
         rooms = Room.objects.all().order_by("-created")
 
-    return render(request, "index.html", {"rooms": rooms, "topics":topics})
+    return render(request, "index.html", {"rooms": rooms, "topics": topics, "room_count": room_count})
+
+
+def login_page(request):
+
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, "Username or password wrong.")
+
+    return render(request, "login_register.html", {"page": page})
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login_page')
+
+
+def register(request):
+
+    form = UserCreationForm()
+
+    if request.method == "POST":
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+
+    return render(request, 'login_register.html', {"form": form})
 
 
 def room(request, id):
     context = Room.objects.get(id=int(id))
-    return render(request, "room.html", {"room":context})
+    return render(request, "room.html", {"room": context})
 
+
+@login_required(login_url='login_page')
 def create(request):
 
     hosts = User.objects.all()
     topics = Topic.objects.all()
 
     if request.method == "POST":
-        
+
         host_id = request.POST.get('host')
         topic_id = request.POST.get('topic')
         name = request.POST.get('name')
-        description  = request.POST.get('desc')
+        description = request.POST.get('desc')
 
         host = User.objects.get(id=host_id)
         topic = Topic.objects.get(id=topic_id)
@@ -50,19 +102,25 @@ def create(request):
 
     return render(request, "create.html", {
         "topics": topics,
-        "hosts":hosts
+        "hosts": hosts
     })
 
+
+@login_required(login_url='login_page')
 def update(request, id):
 
-    room  = Room.objects.get(id=int(id))
+    room = Room.objects.get(id=int(id))
     topics = Topic.objects.all()
 
+    if request.user != room.host:
+        messages.error(request, "You're nor allowed to perform this action.")
+        return
+
     if request.method == "POST":
-        
+
         topic_id = request.POST.get('topic')
         name = request.POST.get('name')
-        description  = request.POST.get('desc')
+        description = request.POST.get('desc')
 
         topic = Topic.objects.get(id=topic_id)
 
@@ -76,14 +134,15 @@ def update(request, id):
 
     return render(request, "update.html", {"room": room, "topics": topics})
 
+
+@login_required(login_url='login_page')
 def delete(request, id):
-    
+
     room = Room.objects.get(id=id)
 
     if request.user != room.host:
         return redirect('index')
-    
+
     if request.method == "POST":
         room.delete()
         return redirect('index')
-    
